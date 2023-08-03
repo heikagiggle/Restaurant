911cd57db060a44c96e6aa1b6b2bf160217c0de9
heikagiggle <116876334+heikagiggle@users.noreply.github.com>
2023-08-03 08:10:24 +0100

    Add files via upload

---
+++ Restaurant/src/components/index.js
@@ -0,0 +1,7 @@
+import SubHeading from "./Subheading/Subheading";
+import Newsletter from "./Footer/Newsletter";
+import FooterOverlay from "./Footer/FooterOverlay";
+import MenuItem from "./MenuItem/MenuItem";
+import Navbar from "./Navbar/Navbar";
+
+export { SubHeading, Newsletter, FooterOverlay, MenuItem, Navbar };
